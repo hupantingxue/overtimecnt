5bfdf105653a72b31847283ad48ba785698d975c
hupantingxue <hupantingxue@126.com>
2014-02-13 15:34:18 +0800

    1. Add query overtime status with specified month; 2. Update overtime status with specified day;

--- overtime.py
+++ overtime.py
@@ -21,20 +21,35 @@ def getTime():
 
 class index:
     def GET(self):
-        mon, day = getTime()
+        _mon, _day = getTime()
+        try:
+            i = web.input()
+            mon = i.month
+        except Exception as e:
+            mon = _mon
         overs = db.select('overtime', where='month=$mon', order='day', vars=locals())
         return render.index(overs)
 
 class over:
     def POST(self):
-        mon, day = getTime()
-        n = db.update('overtime', where='day=$day', overtime=1, vars = locals())
+        mon, myday = getTime()
+        i = web.input()
+        overday = i.overday
+        if "0" == overday:
+            overday = myday
+        n = db.update('overtime', where='day=$overday', overtime=1, vars = locals())
+        #n = db.insert('overtime', overtime=1, day=overday, month=mon)
         raise web.seeother('/')
 
 class normal:
     def POST(self):
+        mon, myday = getTime()
+        i = web.input()
+        overday = i.overday
+        if "0" == overday:
+            overday = myday
         mon, day = getTime()
-        n = db.update('overtime', where='day=$day', overtime=0, vars = locals())
+        n = db.update('overtime', where='day=$overday', overtime=0, vars = locals())
         raise web.seeother('/')
 
 if __name__ == "__main__":
